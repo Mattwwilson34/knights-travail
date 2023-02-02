1235ecbbe9a272d4c1584408145c75a177c97f5d
Matt Wilson <mattwwilson34@gmail.com>
2023-02-02 08:50:39 -0500

    Add: unit tests for gameboard component

--- src/features/gameboard/components/gameboard.tsx
+++ src/features/gameboard/components/gameboard.tsx
@@ -1,19 +1,22 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import get2dArray from '../../../utils/2d-array-generator'
 
 const Gameboard = () => {
 	const gameboardArray: number[][] = get2dArray();
+	// 2D array not required to render gameboard
+	// flattening array reduces need for extra map function 
+	// will need to revisit when game logic introduced
+	const gameboardTiles: number[] = gameboardArray.flat()
 
 	return (
 		<div className="Gameboard" data-testid='Gameboard'>
-			{gameboardArray.map(y => {
+			{gameboardTiles.map(tile => {
 				return (
-					<Fragment key={uuidv4()}>
-						{y.map(x => <div className='Gameboard-Tile' key={uuidv4()}>{x}</div>)}
-					</Fragment>
+					<div className='Gameboard-Tile' key={uuidv4()} data-testid='Gameboard-Tile'>{tile}</div>
 				)
 			})}
+
 		</div>
 	)
 }
