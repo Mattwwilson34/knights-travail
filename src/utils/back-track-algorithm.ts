7c5fa4e00b05ea42ec9e2b15f4bdd5b17dd77313
Matt Wilson <mattwwilson34@gmail.com>
2023-02-04 20:39:51 -0500

    Refactor: algorithm

--- src/utils/back-track-algorithm.ts
+++ src/utils/back-track-algorithm.ts
@@ -1,80 +1,112 @@
 import get2dArray from "./2d-array-generator";
 
-const gameboard = get2dArray()
 
-const backTrack = (gameboard: number[][], startPosition: number[]) => {
-  const outOfBoundsRight: number = gameboard[0].length;
-  const outOfBoundsLeft = -1;
-  const outOfBoundsUp = -1;
-  const outOfBoundsDown: number = gameboard.length;
+const START_X = 0
+const START_Y = 0
 
-  const moveMap = {
-    rightUp: [2, 1],
-    rightDown: [2, -1],
-    leftUp: [-2, 1],
-    leftDown: [-2, -1],
-    upRight: [1, 2],
-    upLeft: [-1, 2],
-    downRight: [1, -2],
-    downLeft: [-1, -2],
-  }
 
-  const totalBoardTiles = gameboard.length * gameboard[0].length;
-  const boardTilesVisited = [startPosition];
-  const numberOfTilesVisited = boardTilesVisited.length;
-  let currentMoveX = startPosition[0]
-  let currentMoveY = startPosition[1]
-  let count = 0;
+function getValidMove(currentPostion: number[], moveSet: number[][], gameboard: (boolean[] | number[])[]) {
+  const [currX, currY] = currentPostion;
+  const positiveOOB = gameboard[0].length;
 
-  // console logging to prevent unused variable linting errors
-  console.log(totalBoardTiles, numberOfTilesVisited)
 
-  while (count < 100) {
-    // chose move from list of moves
-    // convert moveMap to array of values
-    const moveArray = Object.values(moveMap)
+  return moveSet.filter((move) => {
+    const [moveX, moveY] = move;
+    const newX = currX + moveX;
+    const newY = currY + moveY;
 
-    for (let i = 0; i < moveArray.length; i++) {
-      // store last valid move
-      const lastValidMove = boardTilesVisited[boardTilesVisited.length - 1]
-      //select next move
-      const nextMove = moveArray[i]
-      // increment current position to next potential position
-      currentMoveX += nextMove[0]
-      currentMoveY += nextMove[1]
-
-      // check if out of bounds left or right
-      if (currentMoveX <= outOfBoundsLeft || currentMoveX >= outOfBoundsRight) {
-        // reset current positions to last valid move
-        currentMoveX = lastValidMove[0]
-        currentMoveY = lastValidMove[1]
-        continue
-      }
-      // check if out of bounds up or down
-      if (currentMoveY <= outOfBoundsUp || currentMoveY >= outOfBoundsDown) {
-        // reset current positions to last valid move
-        currentMoveX = lastValidMove[0]
-        currentMoveY = lastValidMove[1]
-        continue
-      }
-      // check if move has already been made
-      if (boardTilesVisited.some(tile => (tile[0] === currentMoveX && tile[1] === currentMoveY))) {
-        // reset current positions to last valid move
-        currentMoveX = lastValidMove[0]
-        currentMoveY = lastValidMove[1]
-        continue
-      }
-      else {
-        // if here move is valid
-        // update moves already made array
-        boardTilesVisited.push([currentMoveX, currentMoveY])
-        // break out of for loop
-        break;
-      }
+    // check if out of bounds
+    if (newX < 0 || newX >= positiveOOB || newY < 0 || newY >= positiveOOB) {
+      return false
     }
-    count++
-  }
-  console.log(boardTilesVisited)
+
+    // check if move has already been made
+    if (gameboard[newX][newY]) {
+      return false
+    }
+
+    return true
+  })
 }
 
-export default backTrack
+function updatePosition(currPos: number[], moveMade: number[]) {
+  const [currX, currY] = currPos;
+  const [deltaX, deltaY] = moveMade;
+  const newPosition = [(currX + deltaX), (currY + deltaY)]
+
+  return newPosition
+}
+
+function updateGameboard(currPos: number[], moveMade: number[], moveNumber: number, gameboard: (boolean[] | number[])[]) {
+  const newPosition = updatePosition(currPos, moveMade)
+
+  gameboard[newPosition[0]][newPosition[1]] = moveNumber
+
+  return gameboard
+}
+
+let totalIterations = 0;
+
+function knightMove(moveNumber: number, currPos: number[], moveSet: number[][], gameboard: (boolean[] | number[])[], solution: number[][]) {
+
+  // console.log(moveNumber)
+
+  // base case
+  if (moveNumber === gameboard[0].length ** 2) {
+    console.log('you solved it!')
+    console.table(gameboard);
+    return true
+  }
+  // make copy of game board
+  const gameboardCopy = gameboard.map(row => row.slice())
+
+  // get a list of valid moves from curr position
+  const validMoves = getValidMove(currPos, moveSet, gameboardCopy)
+
+  //loop through valid moves and test
+  for (let i = 0; i < validMoves.length; i++) {
+
+    totalIterations += 1;
+
+    // increase move number
+    moveNumber += 1;
+    // add move to solution array
+    solution.push(updatePosition(currPos, validMoves[i]));
+    // update gameboard
+    updateGameboard(currPos, validMoves[i], moveNumber, gameboardCopy)
+
+    if (!knightMove(moveNumber, solution[solution.length - 1], moveSet, gameboardCopy, solution)) {
+      const lastMove = solution.pop()
+      if (lastMove) {
+
+        if (lastMove[0] === START_X && lastMove[1] === START_Y) {
+          console.log('No solution you have backtracked all the way to your start position.')
+        }
+        gameboardCopy[lastMove[0]][lastMove[1]] = false;
+        moveNumber -= 1;
+      }
+    }
+  }
+  return false
+}
+
+function solveKnightsPath() {
+
+  const gameboard = get2dArray(5, 5)
+
+
+  const solution = [[START_X, START_Y]]
+
+  const moveSet = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
+
+  const moveNumber = 1;
+
+  gameboard[START_X][START_Y] = moveNumber;
+
+  knightMove(moveNumber, solution[0], moveSet, gameboard, solution)
+  console.log(totalIterations)
+  return true;
+}
+
+solveKnightsPath()
+
