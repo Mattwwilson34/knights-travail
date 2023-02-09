ed4609869840741da85500e374fc43f3088f99e2
Matt Wilson <mattwwilson34@gmail.com>
2023-02-08 22:41:27 -0500

    Add: Square component for conditional rendering based on gameboard props

---
+++ src/features/gameboard/components/Square.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+interface SquareProps {
+	x: number,
+	y: number,
+	knightX: number,
+	knightY: number,
+	moveNumber: number,
+	visitedSquares: Set<string>,
+}
+
+const Square = ({ x, y, knightX, knightY, moveNumber, visitedSquares }: SquareProps) => {
+	const isKnight = x === knightX && y === knightY;
+
+
+	const getSquareColor = (x: number, y: number): string => {
+		if (x % 2 === 0 && y % 2 === 1) {
+			return 'black'
+		}
+		if (x % 2 === 1 && y % 2 === 0) {
+			return 'black'
+		}
+		else {
+			return 'white'
+		}
+	}
+
+	return (
+		<div className={`square ${getSquareColor(x, y)}`} data-testid='Gameboard-Square'>
+			{isKnight ? (
+				<span role="img" aria-label="knight" style={{ fontSize: '4rem', color: 'blue' }}>♞</span>
+			) : visitedSquares.has(`${x},${y}`) ? (
+				<span role="img" aria-label="Checkmark" style={{ fontSize: '3rem', color: 'purple' }}>✔️</span>
+			) : moveNumber}
+		</div>
+	);
+}
+
+export default Square
