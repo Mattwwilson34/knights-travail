415b6c44fa9613d85399bc9ffaf38cbfe44bcea6
Matt Wilson <mattwwilson34@gmail.com>
2023-02-08 22:44:38 -0500

    Refactor: removed non essential function paramaters

--- src/utils/knight-tour.ts
+++ src/utils/knight-tour.ts
@@ -1,6 +1,4 @@
-import get2dArray from "./2d-array-generator"
-
-
+import get2dArray from "./get-2D-array";
 
 function knightTour(rows: number, cols: number, rowStart: number, colStart: number): number[][] | boolean {
 
@@ -11,7 +9,36 @@ function knightTour(rows: number, cols: number, rowStart: number, colStart: numb
 
   gameboard[rowStart][colStart] = 1;
 
-  if (findKnightTour(gameboard, rowStart, colStart, 1, rows, cols, pathRow, pathCol)) {
+
+  // validate move util function
+  function validMove(gameboard: number[][], row: number, col: number) {
+    return (row > -1 && row < gameboard[0].length && col > -1 && col < gameboard[0].length && gameboard[row][col] === 0)
+  }
+
+  // recursive function
+  function findKnightTour(gameboard: number[][], row: number, col: number, move: number) {
+    if (move === rows * cols) {
+      return true
+    }
+    else {
+      for (let i = 0; i < pathRow.length; i++) {
+        const rowNew = row + pathRow[i]
+        const colNew = col + pathCol[i]
+        if (validMove(gameboard, rowNew, colNew)) {
+          move++
+          gameboard[rowNew][colNew] = move;
+          if (findKnightTour(gameboard, rowNew, colNew, move)) {
+            return true
+          }
+          move--;
+          gameboard[rowNew][colNew] = 0;
+        }
+      }
+    }
+  }
+  //
+
+  if (findKnightTour(gameboard, rowStart, colStart, 1)) {
     return gameboard
   }
   else {
@@ -20,31 +47,6 @@ function knightTour(rows: number, cols: number, rowStart: number, colStart: numb
   }
 }
 
-function findKnightTour(gameboard: number[][], row: number, col: number, move: number, rows: number, cols: number, pathRow: number[], pathCol: number[]) {
 
-  if (move === rows * cols) {
-    console.table(gameboard)
-    return true
-  }
-  else {
-    for (let i = 0; i < pathRow.length; i++) {
-      const rowNew = row + pathRow[i]
-      const colNew = col + pathCol[i]
-      if (validMove(gameboard, rowNew, colNew)) {
-        move++
-        gameboard[rowNew][colNew] = move;
-        if (findKnightTour(gameboard, rowNew, colNew, move, rows, cols, pathRow, pathCol)) {
-          return true
-        }
-        move--;
-        gameboard[rowNew][colNew] = 0;
-      }
-    }
-  }
-}
-
-function validMove(gameboard: number[][], row: number, col: number) {
-  return (row > -1 && row < gameboard[0].length && col > -1 && col < gameboard[0].length && gameboard[row][col] === 0)
-}
 
 export default knightTour
