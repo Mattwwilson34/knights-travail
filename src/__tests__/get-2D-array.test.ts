76f85dc0b8125a8eecdabd0c93a0c65061bbb270
Matt Wilson <mattwwilson34@gmail.com>
2023-02-08 22:39:04 -0500

    Rename: file

---
+++ src/__tests__/get-2D-array.test.ts
@@ -0,0 +1,31 @@
+import expect from 'expect'
+import get2dArray from '../utils/get-2D-array'
+
+describe('Check 2D array creation', () => {
+	test('2D array is created', () => {
+		const array = get2dArray()
+		expect(array).toBeInstanceOf(Array)
+		expect(array.length).toBe(8)
+		expect(array[0].length).toBe(8)
+	})
+
+	test('2D array contains proper coordinates', () => {
+		const array = get2dArray();
+		expect(typeof array[0][0]).toBe('number')
+	})
+
+	test('2D array function handles varying height and width params', () => {
+		const height = 2;
+		const width = 2;
+		const array = get2dArray(height, width);
+		expect(typeof array[0][0]).toBe('number')
+	})
+
+	test('Error thrown if function params are not numbers', () => {
+		/* eslint-disable  @typescript-eslint/no-explicit-any */
+		const height = 'string' as any
+		/* eslint-disable  @typescript-eslint/no-explicit-any */
+		const width = true as any
+		expect(() => get2dArray(height, width)).toThrowError()
+	})
+})
