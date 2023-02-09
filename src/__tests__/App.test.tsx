11389e2464cce4069f566fde8d940b229ab9c612
Matt Wilson <mattwwilson34@gmail.com>
2023-02-09 12:06:40 -0500

    Add: testing for App for copy and user interactions

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from '../App'
 
 describe('App component', () => {
@@ -8,4 +9,24 @@ describe('App component', () => {
     const AppComponent = screen.getByTestId('App');
     expect(AppComponent).toBeInTheDocument()
   })
+
+  it('Renders all text as expected', () => {
+    const { getByText } = render(<App />)
+    expect(getByText("The Knight's Tour Problem")).toBeInTheDocument()
+    expect(getByText("The Recursive Algorithm Solution")).toBeInTheDocument()
+    expect(getByText("Visual Display of the Algorithm")).toBeInTheDocument()
+  })
+
+  it('Renders the Solution-Gameboard', () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId('Solution-Gameboard')).toBeInTheDocument();
+  });
+
+  it('Renders Gameboard after Button is clicked', async () => {
+    const user = userEvent.setup()
+    render(<App />)
+    await user.click(screen.getByTestId('solve-button'))
+    const GameboardComponent = screen.getByTestId('Gameboard')
+    expect(GameboardComponent).toBeInTheDocument()
+  });
 })
