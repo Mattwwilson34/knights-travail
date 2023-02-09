fbec11b1473c7d416d26349a4a7714e24db14a5a
Matt Wilson <mattwwilson34@gmail.com>
2023-02-08 22:38:01 -0500

    fix: testid naming

--- src/__tests__/Gameboard.test.jsx
+++ src/__tests__/Gameboard.test.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Gameboard from '../features/gameboard'
+import solution from '../utils/mock-solution-array'
 
 describe('Gameboard component', () => {
 	it('Renders the Gamebord on the screen', () => {
-		render(<Gameboard />)
+		render(<Gameboard solution={solution} />)
 		const GameboardComponent = screen.getByTestId('Gameboard');
 		expect(GameboardComponent).toBeInTheDocument()
 	})
 
 	it('Renders the Gameboard tiles on screen', () => {
-		render(<Gameboard />)
-		const GameboardTile = screen.getAllByTestId('Gameboard-Tile')
-		expect(GameboardTile[0]).toBeInTheDocument()
-		expect(GameboardTile[0]).toHaveTextContent(0)
-		expect(GameboardTile[63]).toBeInTheDocument()
-		expect(GameboardTile[63]).toHaveTextContent(0)
+		render(<Gameboard solution={solution} />)
+		const GameboardSquare = screen.getAllByTestId('Gameboard-Square')
+		expect(GameboardSquare[0]).toBeInTheDocument()
+		expect(GameboardSquare[0]).toHaveTextContent('♞')
+		expect(GameboardSquare[63]).toBeInTheDocument()
+		expect(GameboardSquare[63]).toHaveTextContent(13)
 	})
 
 	it('Renders the correct number of gameboard tiles', () => {
-		render(<Gameboard />)
-		const GameboardTile = screen.getAllByTestId('Gameboard-Tile')
-		expect(GameboardTile.length).toBe(64)
+		render(<Gameboard solution={solution} />)
+		const GameboardSquare = screen.getAllByTestId('Gameboard-Square')
+		expect(GameboardSquare.length).toBe(64)
 	})
 
 })
